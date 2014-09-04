18c5d08b4f020eecb6e1f8ecd3cc86058cfbe598
Simon Neveu <simon@hoverstud.io>
2014-09-04 12:05:30 +0100

    Added required js libs

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -1,3 +1,7 @@
+//= require "../bower_components/modernizr/modernizr.js"
+//= require "../bower_components/jquery/dist/jquery.min.js"
+//= require "../bower_components/jquery.transit/jquery.transit.js"
+
 // -------------------------------------------------
 //
 // Home
